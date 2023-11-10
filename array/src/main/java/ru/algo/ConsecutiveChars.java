82abdcd77f7a63b6086fda4997bd2ca8bb4c1a6e
Olga Larina <olga.124@list.ru>
2023-11-10 16:30:11 +0300

    01: array && prefix sum && other

--- array/src/main/java/ru/algo/ConsecutiveChars.java
+++ array/src/main/java/ru/algo/ConsecutiveChars.java
@@ -1,15 +1,12 @@
 package ru.algo;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 1446. Consecutive Characters
  *
  * The power of the string is the maximum length of a non-empty substring that contains only one unique character.
  * Given a string s, return the power of s.
  *
- * https://leetcode.com/problems/contains-duplicate/
+ * https://leetcode.com/problems/consecutive-characters/
  */
 public class ConsecutiveChars {
     public int maxPower(String s) {
